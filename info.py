d4e6bdd3bae257ce35fe6391892d7fde646d973d
Deep Dey <deydeep200821@gmail.com>
2025-04-08 21:40:24 +0530

    info.py

--- info.py
+++ info.py
@@ -1,11 +1,6 @@
-from dotenv import load_dotenv
-import os
 import re
 from os import environ,getenv
 from Script import script
-
-# Load environment variables
-load_dotenv()
 
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
